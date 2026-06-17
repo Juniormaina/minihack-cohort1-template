b7d8a833e202abc4431a17224783781ac0f4a3ab
Juniormaina <zangiffk@gmail.com>
2026-06-17 19:47:30 +0300

    feat: update DeployPayments script and include Avalanche payments project

--- week2/script/DeployPayments.s.sol
+++ week2/script/DeployPayments.s.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
 import "../contracts/PaymentSplitter.sol";
 import "../contracts/Escrow.sol";
-import "../contracts/MockUSDC.sol";
 
-/// @notice Deploy Week 2 contracts to Fuji
-/// @dev source .env && forge script script/DeployPayments.s.sol:DeployPayments --rpc-url $FUJI_RPC_URL --private-key $PRIVATE_KEY --broadcast -vvvv
 contract DeployPayments is Script {
     function run() external {
+        // Load your private key from .env
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployer = vm.addr(deployerPrivateKey);
-        console.log("Deploying with:", deployer);
+
         vm.startBroadcast(deployerPrivateKey);
-        MockUSDC musdc = new MockUSDC();
+
+        // Deploy PaymentSplitter
         PaymentSplitter splitter = new PaymentSplitter();
+
+        // Deploy Escrow
         Escrow escrow = new Escrow();
+
         vm.stopBroadcast();
-        console.log("MockUSDC:       ", address(musdc));
-        console.log("PaymentSplitter:", address(splitter));
-        console.log("Escrow:         ", address(escrow));
     }
 }
+
